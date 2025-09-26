466e6247e1eb0ea6fd53a692278f6cff7e1e5459
ibrahim <ibrahim.imran@iaim.ca>
2025-09-26 14:16:18 +0500

    hello

---
+++ bp_modules/chat/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+/* tslint:disable */
+// This file is generated. Do not edit it manually.
+
+import * as sdk from "@botpress/sdk"
+
+import definition from "./definition"
+
+export default {
+  type: "integration",
+  id: "intver_01K3181E9YVNMKFC76JTF3FP18",
+  uri: undefined,
+  name: "chat",
+  version: ">=0.0.0 <1.0.0",
+  definition,
+} satisfies sdk.IntegrationPackage
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import * as bp from '.botpress'
+
+const bot = new bp.Bot({
+  actions: {},
+})
+
+
+bot.on.message("*", async (props) => {
+  const {
+    conversation: { id: conversationId },
+    ctx: { botId: userId },
+  } = props;
+
+  if (props.message.type !== "text") {
+    await props.client.createMessage({
+      conversationId,
+      userId,
+      tags: {},
+      type: "text",
+      payload: {
+        text: "Sorry, I can only respond to text messages.",
+      },
+    });
+    return;
+  }
+
+  await props.client.createMessage({
+    conversationId,
+    userId,
+    tags: {},
+    type: "text",
+    payload: {
+      text: `You said: ${props.message.payload.text}`,
+    },
+  });
+});
+
+export default bot
